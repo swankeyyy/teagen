ceffe8fc44a6313728a28602e4e58b76f7ba8119
levchuk <swankyyy1@gmail.com>
2024-10-17 11:48:19 +0300

    new endpoint for get tea by id

--- tea/api/tea/crud.py
+++ tea/api/tea/crud.py
@@ -6,11 +6,11 @@ from core.models import Tea
 
 
 async def create_tea(session: AsyncSession, product: ProductCreate) -> Tea:
-    product = Tea(**product.model_dump())
-    session.add(product)
+    item = Tea(**product.model_dump())
+    session.add(item)
     await session.commit()
-    await session.refresh(product)
-    return product
+    await session.refresh(item)
+    return item
 
 
 async def load_tea_image(session: AsyncSession, tea_id: int, image: UploadFile) -> bool:
@@ -22,9 +22,16 @@ async def load_tea_image(session: AsyncSession, tea_id: int, image: UploadFile) 
         with open(file_name, 'wb+') as f:
             f.write(image.file.read())
             f.close()
-        item.image = file_name
+        item.image = file_name[1:]
         await session.commit()
         await session.refresh(item)
-        print(item.image)
         return True
+    return False
+
+
+async def get_tea_by_id(tea_id: int, session: AsyncSession) -> Tea | bool:
+    stmt = select(Tea).where(tea_id == Tea.id)
+    item = await session.scalar(stmt)
+    if item:
+        return item
     return False
--- tea/api/tea/views.py
+++ tea/api/tea/views.py
@@ -3,9 +3,7 @@ from fastapi.exceptions import HTTPException
 from fastapi import APIRouter, Request, Response, UploadFile, File, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
-
 from . import crud
-
 from api.tea.schemas import Product, ProductCreate
 from core.models import db_config
 
@@ -15,13 +13,22 @@ router = APIRouter()
 @router.post("/", response_model=Product, status_code=status.HTTP_201_CREATED)
 async def create_tea_view(product: ProductCreate,
                           session: AsyncSession = Depends(db_config.get_session)):
-    content = await crud.create_tea(session=session, product=product)
-    return content
+    result = await crud.create_tea(session=session, product=product)
+    return result
 
 
 @router.post("/add_image/{tea_id}")
-async def add_tea_image_view(tea_id: int, image: UploadFile, session: AsyncSession = Depends(db_config.get_session)) -> Response:
+async def add_tea_image_view(tea_id: int, image: UploadFile,
+                             session: AsyncSession = Depends(db_config.get_session)) -> Response:
     result = await crud.load_tea_image(session=session, image=image, tea_id=tea_id)
     if result:
         return Response(status_code=201)
     raise HTTPException(500, detail='wrong id')
+
+
+@router.get("/{tea_id}", response_model=Product, status_code=status.HTTP_200_OK)
+async def get_tea_by_id_view(tea_id: int, session: AsyncSession = Depends(db_config.get_session)):
+    result = await crud.get_tea_by_id(session=session, tea_id=tea_id)
+    if result:
+        return result
+    raise HTTPException(status_code=404, detail='wrong id')
