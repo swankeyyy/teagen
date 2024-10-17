110448a5cbae82f5626cb1dbf352e6183bd87e59
levchuk <swankyyy1@gmail.com>
2024-10-17 12:39:14 +0300

    create endpoint for media

--- tea/main.py
+++ tea/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
+from starlette.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from api import router as api_router
 from contextlib import asynccontextmanager
 from core.models import db_config
@@ -12,6 +14,13 @@ async def lifespan(app: FastAPI):
 
 
 main_app = FastAPI(lifespan=lifespan)
+
+
+@main_app.get("/media/{img}", tags=["media"], summary="Response Image by path")
+async def media_view(img: str) -> FileResponse:
+    """response image by path from item db"""
+    print(img)
+    return FileResponse(f'media/{img}', status_code=200)
 
 main_app.include_router(api_router)
 
